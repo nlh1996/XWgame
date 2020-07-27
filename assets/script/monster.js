527ece8e1c05183e5e993593b8d21f1ec5e7eb03
nlh1996 <1217286494@qq.com>
2020-07-27 17:54:11 +0800

    111

--- assets/script/monster.js
+++ assets/script/monster.js
@@ -16,12 +16,24 @@ cc.Class({
     // onLoad () {},
 
     start () {
-      let act1 = cc.moveTo(1.5, cc.v2(this.node.x+200, this.node.y))
-      let act2 = cc.moveTo(1.5, cc.v2(this.node.x-200, this.node.y))
+      this.ai()
+    },
+
+    ai() {
+      var d = 1.5
+      if(this.node.x > 150) {
+        var act1 = cc.moveTo(d, cc.v2(330, this.node.y))
+      }else {
+        var act1 = cc.moveTo(d, cc.v2(this.node.x + 150, this.node.y))
+      }
+      if (this.node.x > -200) {
+        var act2 = cc.moveTo(d, cc.v2(this.node.x - 150, this.node.y))
+      }else {
+        var act2 = cc.moveTo(d, cc.v2(-330, this.node.y))
+      }
       let seq = cc.sequence(act1, act2)
       let repeat = cc.repeatForever(seq)
       this.node.runAction(repeat)
-    },
-
+    }
     // update (dt) {},
 });
